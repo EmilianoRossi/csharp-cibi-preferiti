b6a58fce118bafb422b1f7d4a3f513d99e0a8ad6
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-01 14:41:16 +0200

    esercizio 1 bonus : Stampare elemento in mediana in array [N pari] completato e commentato

---
+++ Bonus-CibiPreferiti/Bonus-CibiPreferiti/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Bonus esercizio cibi preferiti
+//Dichiaro l'array con i miei cibi preferiti
+string[] arrayCibiPreferiti = { "Linguine al nero di seppia", "Trippa alla romana", "Tortellini burro e salvia", "Gamberi crudi", "Tartare di tonno", "Totani alla piastra"};
+
+//Esercizio numero 1 : stampa la lunghezza dell'array
+Console.WriteLine("La lista è composta da " + arrayCibiPreferiti.Length + " piatti");
+
+//Scorro l'array con un ciclo 
+//esercizio numero 2 : Stampa la classifica con posizione dei cibi
+
+Console.WriteLine("Classifica cibi preferiti: ");
+for (int i = 0; i < arrayCibiPreferiti.Length; i++)
+{
+    int posizioneInClassifica = i + 1;
+    Console.WriteLine(posizioneInClassifica + " " + " " + arrayCibiPreferiti[i]);
+}
+
+//esercizio numero 3 : stampa il tuo cibo top
+Console.WriteLine("Cibo top: " + arrayCibiPreferiti[0]);
+
+//esercizio numero 4 : stampa il tuo cibo preferito ma non troppo
+//inserisco in una variabile la lunghezza della stringa-1 in modo da prendere l'indicatore dell'ultima posizione del mio array
+int ultimoElementoArray = arrayCibiPreferiti.Length - 1;
+
+//inserisco l'elemento acquisito in precedenza nell'array per prendere l'ultimo elemento dell'array
+Console.WriteLine("Il mio cibo preferito ma non troppo è: " + arrayCibiPreferiti[ultimoElementoArray]);
+
+//BONUS
+//esercizio 1 bonus : Stampare elemento in mediana 
+int elementoMedianaArray = arrayCibiPreferiti.Length / 2 - 1;
+Console.WriteLine("Cibo preferito di metà classifica: " + arrayCibiPreferiti[elementoMedianaArray]);
