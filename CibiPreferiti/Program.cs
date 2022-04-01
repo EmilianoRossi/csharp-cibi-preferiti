c9e414cec879f86535d32d79898e739339a26100
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-01 14:04:16 +0200

    Add project files.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Dichiaro l'array con i miei cibi preferiti
+string[] arrayCibiPreferiti = { "Linguine al nero di seppia", "Trippa alla romana", "Tortellini burro e salvia", "Gamberi crudi", "Tartare di tonno", "Totani alla piastra" };
+
+//Esercizio numero 1 : stampa la lunghezza dell'array
+Console.WriteLine("La lista è composta da " + arrayCibiPreferiti.Length + " piatti");
+
+//Scorro l'array con un ciclo
